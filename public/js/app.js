d722a45065d8da33510969e33e58539435ccbb00
Dustin Crawford <crawforddustins@yahoo.com>
2019-02-28 16:01:42 -0600

    Deleting CSS

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ $(".addArticle").on("click",function(){
         window.location.href = "/";
     });
 })
+
+// $(".addNote").on("click",function(){
+//     $.ajax({url:"/addNoteForm/:id",method:"post",data:this.dataset}).append(".pComments").then(function(result){
+//         window.location.href = "/";
+//     });
+// })
